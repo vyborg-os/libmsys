cb2a13b46bba1dce9cdc4efcd797d79acb54dcff
vyborg <41639842+vyborg-os@users.noreply.github.com>
2025-07-01 13:12:03 +0100

    new Update server.js

--- server.js
+++ server.js
@@ -15,7 +15,8 @@ const port = process.env.PORT || 5000;
 // Middleware
 // Configure CORS to allow requests from any origin
 app.use(cors({
-  origin: '*', // Allow all origins
+  origin: 'https://libmsys-mrez.vercel.app', // Your frontend URL
+  credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
 }));
